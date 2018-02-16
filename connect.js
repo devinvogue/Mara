a4a578893ad59a7f6dccd4de51b315758b549258
mhandie <anokpara@yahoo.com>
2018-02-16 16:00:13 +0000

    added connect page

---
+++ connect.js
@@ -0,0 +1,9 @@
+        function myFunction()
+         {var x;
+           var person = prompt ("please enter your name", "Full Name");
+          var y;
+           var person = prompt ("please enter your phone number", eg +233500386321);
+        if (person != null) {
+               x = "Hello" + person +"! Please wait..You are being logged in...";
+           }
+           } 
